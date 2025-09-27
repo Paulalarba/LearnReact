7b619d086f1f2ba60fd173ca2190280f4a2a5558
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-28 04:28:48 +0800

    good night tug nako

--- LearnReact/src/hero.jsx
+++ LearnReact/src/hero.jsx
@@ -1,15 +1,15 @@
 import React from "react";
 
-function hero(){
+function Hero(){
     return(
-        <section style={{ textAlign: "center", padding: "100px 20px", background: "#242424"}}>
+        <section style={{ textAlign: "center", padding: "100px 20px", background: "#242424", color: "white" }}>
         <h1>WELCOME TO RENT AN ERRAND CEBU</h1>
-        <P>Rent-an-Errand Cebu is a local online platform that connects busy people who need help 
+        <p>Rent-an-Errand Cebu is a local online platform that connects busy people who need help 
         with everyday tasks to trusted locals who are ready to earn by offering their skills. Whether 
         it’s cleaning, buying groceries, running errands, or simple household chores, our goal is to 
         make life easier for Cebu City residents while creating opportunities for others to earn extra 
-        income—fast, simple, and hassle-free.</P>
+        income—fast, simple, and hassle-free.</p>
     </section>
     );
 }
-export default hero;
+export default Hero;
