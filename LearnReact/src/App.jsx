c26fc7d850c133c76aee829624696b59c8c4d015
PAUL ALARBA <alarbapaulax@gmail.com>
2025-09-28 04:14:22 +0800

    wanako kasabott

--- LearnReact/src/App.jsx
+++ LearnReact/src/App.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import HelloWorld from "./helloworld"; // import your component
+import Hero from "./hero";
 
 function App() {
   return (
     <div>
       <HelloWorld /> {/* this will display your message */}
+      <Hero />
     </div>
   );
 }
---
+++ LearnReact/src/hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function hero(){
+    return(
+        <section style={{ textAlign: "center", padding: "100px 20px", background: "#242424"}}>
+        <h1>WELCOME TO RENT AN ERRAND CEBU</h1>
+        <P>Rent-an-Errand Cebu is a local online platform that connects busy people who need help 
+        with everyday tasks to trusted locals who are ready to earn by offering their skills. Whether 
+        it’s cleaning, buying groceries, running errands, or simple household chores, our goal is to 
+        make life easier for Cebu City residents while creating opportunities for others to earn extra 
+        income—fast, simple, and hassle-free.</P>
+    </section>
+    );
+}
+export default hero;
